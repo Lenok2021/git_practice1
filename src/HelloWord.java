49b3269566e11481b352540fc14a9b5be9df2f27
cross <crosstown11223@yahoo.com>
2022-01-10 14:45:08 -0500

    work 3 is done  and to be pushed to GitHub

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,9 +1,9 @@
 public class HelloWord {
 
-
- // doing some work here(you can type anything you want)
+    // doing some work here(you can type anything you want)
 // doing awesome work 2!
-
+// doing awesome work3!
+// doing awesome work 3  is  done and ready tobe committed!
 
 }
 
